5baa73c87c8acf355d4a8c32bdd3195d4e595235
arifur-rahman <56752168+Md-Arifur@users.noreply.github.com>
2019-11-22 22:29:05 +0600

    Google map, Device location, Location permission.

---
+++ app/src/main/java/com/bitm/googlemap/MainActivity.java
@@ -0,0 +1,32 @@
+package com.bitm.googlemap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng dhakaLatLng = new LatLng(23.7984575, 90.4030952);
+        googleMap.addMarker(new MarkerOptions().position(dhakaLatLng));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dhakaLatLng,16));
+
+    }
+}
